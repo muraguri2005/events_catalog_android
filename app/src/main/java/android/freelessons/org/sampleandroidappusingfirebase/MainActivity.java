7acc13e5d8ec3178f08b66cddbc4a6f42915797c
Richard Gathogo <muraguri2005@gmail.com>
2016-08-04 09:43:15 -0400

    add fragments, and roboguice

--- app/src/main/java/android/freelessons/org/sampleandroidappusingfirebase/MainActivity.java
+++ app/src/main/java/android/freelessons/org/sampleandroidappusingfirebase/MainActivity.java
@@ -1,13 +1,18 @@
 package android.freelessons.org.sampleandroidappusingfirebase;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
 
+@ContentView(R.layout.activity_main)
+public class MainActivity extends FragmentActivity {
+
+    @InjectView(R.id.toolbar) Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
